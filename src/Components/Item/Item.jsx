57c4c7a3940a47a4c8b578cc8094b3261d9ee40f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-24 02:21:53 +0600

    card edited

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import { BsBookmark } from 'react-icons/bs';
 
 const Item = ({ item }) => {
   return (
-    <div className="p-5 rounded-md my-5 bg-gray-100 ">
+    <div className="p-5 rounded-md my-5 bg-gray-50 ">
       <div className="full-card  w-full  ">
         <img className="rounded-lg mx-auto w-full" src={item.CoverImage} alt="" />
         <div className="info">
           <div className="full-profile flex justify-between">
             <div className="profile-info flex ">
               {" "}
-              <div className="main-profile flex">
-                <div className="image"><img className="w-14" src={item.AuthorImage} alt="" /></div>
-                <div className="profile-data">
-                  <div className="name">{item.Author}</div>
+              <div className="main-profile flex p-2">
+                <div className="image p-1"><img className="w-14" src={item.AuthorImage} alt="" /></div>
+                <div className="profile-data m-2">
+                  <div className="name text-xl font-semibold">{item.Author}</div>
                   <div className="time text-gray-600">{item.PublishedAt}</div>
                 </div>
               </div>
             </div>
-            <div className="bookmark-btn text-gray-600">
-                <p>{item.ReadTime} min</p>
-                <button></button>
+            <div className="bookmark-btn text-gray-600 flex ">
+                <p className="my-auto">{item.ReadTime} min</p>
+                <button ><BsBookmark/></button>
             </div>
           </div>
           <div className="tittle text-2xl font-semibold">{item.Title}</div>
